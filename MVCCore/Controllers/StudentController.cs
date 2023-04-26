0071083f9fd5ffd05dd58f252846656eab40e460
ngup-j <ditthapong13529@gmail.com>
2023-04-26 16:59:54 +0700

    Add project files.

---
+++ MVCCore/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MVCCore.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("คะแนนสอบวิชาคอมพิวเตอร์");
+        }
+
+        public IActionResult Details()
+        {
+            return Content("รายละเอียดคะแนนสอบ");
+        }
+        public IActionResult ShowScore(int id)
+        {
+            return Content($"คะแนนสอบของนักเรียน เลขที่ {id}");
+        }
+    }
+}
